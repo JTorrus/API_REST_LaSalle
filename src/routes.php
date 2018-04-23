a8b68a667c4891d11a6d1839abe4692270543b7c
JTorrus <jtorrusgalindo@gmail.com>
2018-04-23 13:02:36 +0200

    Routes created, todo: implementation with controller

--- src/routes.php
+++ src/routes.php
@@ -5,10 +5,46 @@ use Slim\Http\Response;
 
 // Routes
 
-$app->get('/[{name}]', function (Request $request, Response $response, array $args) {
-    // Sample log message
-    $this->logger->info("Slim-Skeleton '/' route");
+$app->get('/', function (Request $request, Response $response) {
 
-    // Render index view
-    return $this->renderer->render($response, 'index.phtml', $args);
+});
+
+$app->get('/getAll', function (Request $request, Response $response) {
+
+});
+
+$app->get('/getPublic', function (Request $request, Response $response) {
+
+});
+
+$app->get('/getOne/{id}', function (Request $request, Response $response, array $args) {
+
+});
+
+$app->post('/insert/{id}', function (Request $request, Response $response, array $args) {
+
+});
+
+$app->delete('remove/{id}', function (Request $request, Response $response, array $args) {
+
+});
+
+$app->get('getAllWithTag/{tag}', function (Request $request, Response $response, array $args) {
+
+});
+
+$app->put('addTagOnNote/{id}/{tag}', function (Request $request, Response $response, array $args) {
+
+});
+
+$app->delete('deleteTagOnNote/{id}', function (Request $request, Response $response, array $args) {
+
+});
+
+$app->put('/updateNote/{id}[/{params:.*}]', function (Request $request, Response $response, array $args) {
+
+});
+
+$app->put('/flipPrivate/{id}', function (Request $request, Response $response, array $args) {
+
 });
